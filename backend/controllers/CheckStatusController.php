3c91760671268af17d091e0f6f1f5aaca6bab596
tsydenov <tsydenovs@mail.ru>
2024-04-29 20:04:39 +0700

    Added new controller

---
+++ backend/controllers/CheckStatusController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace backend\controllers;
+
+use backend\models\UrlStatus;
+use backend\components\UrlChecker;
+use DateTime;
+use Yii;
+use yii\httpclient\Client;
+use yii\web\Controller;
+
+class CheckStatusController extends Controller
+{
+    public $urlStatusClass = 'backend\models\UrlStatus';
+    public $enableCsrfValidation = false;
+
+    public function actionIndex()
+    {
+        $urls = UrlStatus::find()->all();
+        return $this->asJson($urls);
+    }
+
+    public function actionCreate()
+    {
+        $request = Yii::$app->request;
+        $urls = $request->getBodyParam('url');
+
+        $response = Yii::$app->urlChecker->check($urls);
+
+        return $this->asJson(["codes" => $response]);
+    }
+}
