42b4f50b154db05d92df05f11325d6cce47fbea9
tsydenov <tsydenovs@mail.ru>
2024-05-03 17:55:24 +0700

    Removed explicit filling of created_at, updated_at from UrlChecker

--- backend/components/UrlChecker.php
+++ backend/components/UrlChecker.php
@@ -3,7 +3,6 @@
 namespace backend\components;
 
 use backend\models\UrlStatus;
-use DateTime;
 use Yii;
 use yii\base\Component;
 use yii\httpclient\Client;
@@ -30,32 +29,12 @@ class UrlChecker extends Component
                     } else {
                         $statusCode = $this->getStatusCodeFromUrl($url);
                         $urlStatus->status_code = $statusCode;
-                        $urlStatus->updated_at = Yii::$app
-                            ->formatter
-                            ->asDatetime(
-                                new DateTime('now'),
-                                'php:Y-m-d H:i:s'
-                            );
                     }
                     $urlStatus->query_count += 1;
                 } else {
                     $urlStatus = new UrlStatus();
                     $urlStatus->url = $url;
                     $urlStatus->hash_string = $hash_string;
-
-                    $urlStatus->created_at = Yii::$app
-                        ->formatter
-                        ->asDatetime(
-                            new DateTime('now'),
-                            'php:Y-m-d H:i:s'
-                        );
-                    $urlStatus->updated_at = Yii::$app
-                        ->formatter
-                        ->asDatetime(
-                            new DateTime('now'),
-                            'php:Y-m-d H:i:s'
-                        );
-
                     $statusCode = $this->getStatusCodeFromUrl($url);
                     $urlStatus->status_code = $statusCode;
                     $urlStatus->query_count = 1;
