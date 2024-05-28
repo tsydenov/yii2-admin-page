c4b2ff17dc16a2bc4ad7770c25dad9e84e6ade89
tsydenov <tsydenovs@mail.ru>
2024-05-28 13:15:56 +0700

    Added fixture data for url_status table

---
+++ common/fixtures/data/url_status.php
@@ -0,0 +1,33 @@
+<?php
+
+use backend\components\UrlChecker;
+use yii\db\Expression;
+
+$status_codes = [
+    200, 201, 202,
+    300, 301, 302, 303, 304, 305,
+    400, 401, 402, 403, 404, 405,
+    500, 501, 502, 503,
+];
+
+$urls = array_map(function ($status_code) {
+    return 'https://httpstat.us/' . $status_code;
+}, $status_codes);
+
+$urlChecker = new UrlChecker();
+
+$responses = array_map([$urlChecker, 'getStatusCodeFromUrl'], $urls);
+
+$fixtures = [];
+for ($i = 0; $i < count($urls); $i++) {
+    $fixtures[$status_codes[$i]] = [
+        'url' => $urls[$i],
+        'hash_string' => md5($urls[$i]),
+        'status_code' => $responses[$i],
+        'query_count' => 1,
+        'created_at' => new Expression('NOW()'),
+        'updated_at' => new Expression('NOW()'),
+    ];
+}
+
+return $fixtures;
